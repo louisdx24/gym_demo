b7066229c30b4b0cc912e118a3a9bf7b21eeb251
louis <louisdx24@icloud.com>
2024-01-27 21:55:40 +0800

    將結果與評語輸出到網頁

--- js/bmi.js
+++ js/bmi.js
@@ -1,10 +1,25 @@
 const userNameE1 = document.querySelector("#username");
 const heightE1 = document.querySelector("#height");
 const weightE1 = document.querySelector("#weight");
+const bmiE1 = document.querySelector("#bmi");
+const commentE1 = document.querySelector("#comment");
 
-console.log(userNameE1, heightE1, weightE1)
+function cleanForm() {
+    userNameE1.value = "";
+    heightE1.value = "";
+    weightE1.value = "";
+    bmiE1.innerText = "BMI";
+    commentE1.innerText = "體位";
+}
 
-console.log(getBMI(heightE1.value, weightE1.value));
+
+function getBMI(height, weight) {
+    let bmi = weight / (height / 100) ** 2;
+    if (isNaN(bmi)) {
+        return ("數值錯誤");
+    }
+    return bmi.toFixed(2);
+}
 
 function calBMI() {
     let height = heightE1.value;
@@ -15,14 +30,26 @@ function calBMI() {
         return;
     }
 
+    //取得 BMI
     let bmi = getBMI(height, weight);
-    console.log(bmi);
-}
-
-function getBMI(height, weight) {
-    let bmi = weight / (height / 100) ** 2;
-    if (isNaN(bmi)) {
-        return ("數值錯誤");
+    //取得評語
+    let comment;
+    if (bmi < 18.5) {
+        comment = "體重過輕";
+    } else if (bmi < 24) {
+        comment = "正常範圍";
+    } else if (bmi < 27) {
+        comment = "過重";
+    } else if (bmi < 30) {
+        comment = "輕度肥胖";
+    } else if (bmi < 35) {
+        comment = "中度肥胖";
+    } else {
+        comment = "重度肥胖";
     }
-    return bmi.toFixed(2);
+
+    console.log(bmi, comment);
+
+    bmiE1.innerText = "BMI:" + bmi;
+    commentE1.innerText = comment;
 }
